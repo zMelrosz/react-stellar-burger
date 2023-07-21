245b5d5f92636ee414c28310c380e4c9ba7a94a3
Igor Khomenko <zmelrosz@yandex.ru>
2023-07-21 19:02:16 +0300

    modal window pt.1.1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,6 @@
 import modalStyles from "./Modal.module.css";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import doneImage from "../../images/done.svg";
 
 const Modal = () => {
     return (
@@ -7,6 +8,8 @@ const Modal = () => {
             <div className={modalStyles.closeIcon}><CloseIcon  type="primary" /></div>
             <p className=" text text_type_digits-large mt-30">034536</p>
             <p className="text text_type_main-medium mt-8">идентификатор заказа</p>
+            <img className="mt-15" src={doneImage}  alt="done" />
+            <p className="text text_type_main-small">Ваш заказ начали готовить</p>
         </div>
         
     )
